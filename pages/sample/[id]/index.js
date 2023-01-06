a8b3d9d7a36335db375eab9fbfb0a6dc46f6acea
nicolumibao <nico.halcyondigital@gmail.com>
2023-01-06 20:28:40 +0800

    chore: fix build
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- pages/sample/[id]/index.js
+++ pages/sample/[id]/index.js
@@ -3,6 +3,7 @@ const S1 = dynamic(() => import('@/components/slices/S1'))
 import { paths, props } from '@/lib/props/page'
 export const getStaticPaths = paths
 export const getStaticProps = props
+
 export default function Home({ page }) {
   return (
     <>

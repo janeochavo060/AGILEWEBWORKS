47b8fd760febcd4509a725e21e7ab815bcdf26bf
nicolumibao <nico.halcyondigital@gmail.com>
2023-01-06 20:08:30 +0800

    chore: update request
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- pages/sample/[id]/index.js
+++ pages/sample/[id]/index.js
@@ -1,18 +1,9 @@
-
 import dynamic from 'next/dynamic'
 const S1 = dynamic(() => import('@/components/slices/S1'))
-
 import { paths, props } from '@/lib/props/page'
 export const getStaticPaths = paths
 export const getStaticProps = props
 export default function Home({ page }) {
-
-  // const { data } = USERAPI.getUserSwr('', {
-  //   onSuccess: () => {
-  //     console.log('asd')
-  //   }
-  // })
-
   return (
     <>
       <S1/>

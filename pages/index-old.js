17707c5cf3dfccb06afe8a8775dd9084c923abf9
Zarley Vedad <zpvedad.halcyondigital@gmail.com>
2023-01-06 20:33:56 +0800

    chore: fix build

--- pages/index-old.js
+++ pages/index-old.js
@@ -2,12 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const router = useRouter();
   return (
     <>
       <Head>
--- pages/sample/[id]/index.js
+++ pages/sample/[id]/index.js
@@ -1,22 +1,32 @@
 
-import dynamic from 'next/dynamic'
-const S1 = dynamic(() => import('@/components/slices/BigTitleWithSuperTitle'))
+// import dynamic from 'next/dynamic'
+// const S1 = dynamic(() => import('@/components/slices/BigTitleWithSuperTitle'))
 
-import { paths, props } from '@/lib/props/page'
-export const getStaticPaths = paths
-export const getStaticProps = props
-export default function Home({ page }) {
+// import { paths, props } from '@/lib/props/page'
+// export const getStaticPaths = paths
+// export const getStaticProps = props
+// export default function Home({ page }) {
 
-  // const { data } = USERAPI.getUserSwr('', {
-  //   onSuccess: () => {
-  //     console.log('asd')
-  //   }
-  // })
+//   const { data } = USERAPI.getUserSwr('', {
+//     onSuccess: () => {
+//       console.log('asd')
+//     }
+//   })
 
+//   return (
+//     <>
+//       <S1/>
+//       {page.name}
+//     </>
+//   )
+// }
+import React, { useState, useEffect } from 'react';
+
+const Sample = () => {
   return (
     <>
-      <S1/>
-      {page.name}
     </>
   )
 }
+
+export default Sample

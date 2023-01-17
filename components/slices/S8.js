cc9702d2867ce5c117fba001f10796c9693000dc
Zarley Vedad <zpvedad.halcyondigital@gmail.com>
2023-01-17 16:20:28 +0800

    fix: small

--- components/slices/S8.js
+++ components/slices/S8.js
@@ -8,7 +8,7 @@ export default function S8 ({ slice }) {
     <div className='m-8'>
       <div className="video-wrapper">
         <ReactPlayer
-          className=""
+          className="player"
           url="https://www.youtube.com/watch?v=8dGdIcyDk1w&ab_channel=edureka%21"
           width="100%"
           height="100%"

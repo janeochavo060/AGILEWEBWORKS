c02c6f586f49695028e8269dd5e78f781f155a79
Zarley Vedad <zpvedad.halcyondigital@gmail.com>
2023-01-17 23:18:44 +0800

    fix: default layout

--- components/layouts/DefaultLayout.js
+++ components/layouts/DefaultLayout.js
@@ -4,10 +4,10 @@ import Footer from "@/components/partials/Footer";
 const DefaultLayout = (props) => {
   return (
     <>
-      <div className="">
+      <div  className={`sticky top-0 md:py-4 py-0 z-50 w-full transition duration-600 ease-in-out bg-transparent backdrop-blur`}>
         <Menu />
       </div>
-      <div className="h-full lg:my-0 my-8">
+      <div className="h-full -my-28">
         {props.children}
       </div>
       <div>

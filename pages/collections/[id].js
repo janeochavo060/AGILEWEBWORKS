4e5721b32b247418c3464d654a0e24f71fc403c2
nicolumibao <nico.halcyondigital@gmail.com>
2023-01-20 14:43:38 +0800

    chore: add form
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- pages/collections/[id].js
+++ pages/collections/[id].js
@@ -1,24 +1,24 @@
 import Head from 'next/head'
-import dynamic from 'next/dynamic'
-import { paths, props } from '@/lib/props/page'
-export const getStaticPaths = paths
-export const getStaticProps = props
+// import dynamic from 'next/dynamic'
+// import { paths, props } from '@/lib/props/page'
+// export const getStaticPaths = paths
+// export const getStaticProps = props
 
 export default function Home({ page }) {
   // console.log(page)
-  let slices = [
-    {
-      key: 'S1',
-      data: {}
-    },
-    {
-      key: 'S2',
-      data: {}
-    },
-  ]
-  const sliceComponents = slices.map((e) => {
-    return dynamic(() => import('@/components/slices/' + e.key))
-  })
+  // let slices = [
+  //   {
+  //     key: 'S1',
+  //     data: {}
+  //   },
+  //   {
+  //     key: 'S2',
+  //     data: {}
+  //   },
+  // ]
+  // const sliceComponents = slices.map((e) => {
+  //   return dynamic(() => import('@/components/slices/' + e.key))
+  // })
 
   return (
     <>
@@ -29,9 +29,9 @@ export default function Home({ page }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {sliceComponents.map((SliceComponent, key) => (
+      {/* {sliceComponents.map((SliceComponent, key) => (
          <SliceComponent key={key} slice={slices[key]} />
-      ))}
+      ))} */}
 
     </>
   )

d4553217fa448062240eabfd917a2b7b15b2a879
nicolumibao <nico.halcyondigital@gmail.com>
2023-01-23 13:36:11 +0800

    chore: add build script
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,10 @@ import '@/styles/globals.css'
 import "../styles/main.css";
 import DefaultLayout from "@/components/layouts/DefaultLayout";
 import Header from "@/components/partials/Header"
+// import globalData from "@/lib/preBuildScripts/static/globalData.json";
+
 export default function App({ Component, pageProps }) {
+  // console.log(globalData, 'Global')
   return (
     <>
       <Header/>

80e18ef24822381fc44044aacfd995e344e3ceaa
nicolumibao <nico.halcyondigital@gmail.com>
2023-01-31 14:01:24 +0800

    chore: add page 5
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

---
+++ components/page5/S1.js
@@ -0,0 +1,7 @@
+const S1 = () => {
+
+  return (
+    <div>page 5</div>
+  );
+};
+export default S1;
---
+++ pages/page-5.js
@@ -0,0 +1,11 @@
+
+import S1 from '@/components/page5/S1'
+const Website = ({}) => {
+  return (
+    <>
+      <S1/>
+    </>
+  );
+}
+
+export default Website

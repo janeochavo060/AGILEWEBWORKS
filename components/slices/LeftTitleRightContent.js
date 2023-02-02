d81e60610afcfa2ee0bbf36305dcef75b6571b71
Mariel <mbmendoza.halcyondigital@gmail.com>
2023-02-02 09:43:45 +0800

    fix: minor changes

--- components/slices/LeftTitleRightContent.js
+++ components/slices/LeftTitleRightContent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TitleContentBlock from "../partials/TitleContentBlock";
 
 export default function LeftTitleRightContent({ slice }) {
   return (
@@ -11,12 +12,10 @@ export default function LeftTitleRightContent({ slice }) {
       className="px-8 xxl:px-0"
     >
       <div className="w-full max-w-screen-xl mx-auto grid lg:grid-cols-3 xl:grid-cols-4 items-center gap-8 py-6 md:py-16">
-        <h2 className="font-bold text-4xl">{slice?.main?.title}</h2>
-        <div
-          className="lg:col-span-2 xl:col-span-3"
-          dangerouslySetInnerHTML={{
-            __html: slice?.main?.content,
-          }}
+        <TitleContentBlock
+          slice={slice}
+          titleClass="text-4xl"
+          contentClass="lg:col-span-2 xl:col-span-3"
         />
       </div>
     </div>

6c060e8c29816cfc6e50d82fa779cae14adf8134
nicolumibao <nico.halcyondigital@gmail.com>
2023-02-03 12:32:56 +0800

    chore: add basic image block
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- components/slices/SingleImageTextBasic.js
+++ components/slices/SingleImageTextBasic.js
@@ -1,9 +1,27 @@
 import React from "react";
 import Image from "next/image";
-import TitleContentBlock from "@/components/partials/TitleContentBlock";
-import Link from "next/link";
 export default function SingleImageText({ slice }) {
   // SINGLE IMAGE WITH TEXT BLOCK
+  console.log(slice)
+
+  const getClass = (data) => {
+    const main = slice?.main
+    if (main) {
+      if (data === 'image') {
+        switch (main?.image_division) {
+          case '1/2': return 'lg:min-h-[460px] lg:min-w-[460px]'
+          case '1/4': return 'lg:min-h-[460px] lg:min-w-[460px]'
+          case '3/4': return 'lg:min-h-[460px] lg:min-w-[460px]'
+        }
+      } else {
+        switch (main?.image_division) {
+          case '1/2': return 'lg:min-h-[460px] lg:min-w-[460px]'
+          case '1/4': return 'lg:min-h-[460px] lg:min-w-[460px]'
+          case '3/4': return 'lg:min-h-[460px] lg:min-w-[460px]'
+        }
+      }
+    }
+  }
 
   return (
     <div className="trapezoid-right">
@@ -15,31 +33,18 @@ export default function SingleImageText({ slice }) {
             src={`https://s3.ap-southeast-1.amazonaws.com/halcyon-agile-saas-platform-boilerplate/${slice?.main?.image}`}
             width={460}
             height={460}
-            className="w-full rounded-r-xxl rounded-bl-xxl lg:min-h-[460px] lg:min-w-[460px] object-cover"
+            className={`w-full rounded-r-xxl rounded-bl-xxl object-cover ${getClass('image')}`}
           />
-          <div className="flex flex-col justify-center gap-4 text-white">
-            <TitleContentBlock slice={slice} />
+          <div className={`${getClass('content')}`}>
+            <h2>
+              {slice?.main?.title || ""} 
+            </h2>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: slice?.main?.content || "",
+              }}
+            />
           </div>
-        </div>
-      </div>
-    </div>
-  );
-  return (
-    <div
-      className="trapezoid-right"
-      style={{ backgroundColor: slice?.main?.bg_color }}
-    >
-      <div className="w-[95%] md:w-full my-8 max-w-screen-xl mx-auto px-[7%] xl:px-0 flex flex-col lg:flex-row items-center gap-8 py-8">
-        <Image
-          alt=""
-          blurDataURL={`https://s3.ap-southeast-1.amazonaws.com/halcyon-agile-saas-platform-boilerplate/${slice?.main?.image}`}
-          src={`https://s3.ap-southeast-1.amazonaws.com/halcyon-agile-saas-platform-boilerplate/${slice?.main?.image}`}
-          width={460}
-          height={460}
-          className="w-full rounded-r-xxl rounded-bl-xxl lg:min-h-[560px] lg:min-w-[560px] object-cover"
-        />
-        <div className="p-8 text-white space-y-4">
-          <TitleContentBlock slice={slice} />
         </div>
       </div>
     </div>

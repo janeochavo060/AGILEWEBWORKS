d0568b84847c0a0fdf60d4a87ede0c0ac88e2c30
Dion <diome.halcyonwebdesign@gmail.com>
2023-02-06 17:22:36 +0800

    class container

--- components/page5/slices/ImageTitleDescription.js
+++ components/page5/slices/ImageTitleDescription.js
@@ -5,7 +5,7 @@ export const ImageTitleDescription = ({slice, post}) => {
   
   return (
     <section className={`block py-[100px] w-full overflow-hidden slice slice-type-${slice.sliceType}`}>
-        <div className="container mx-auto">
+        <div className="max-w-screen-xl mx-auto">
                 <div className={`block__image ${slice.alignmentRight === 0 ? 'float-left pr-[100px]' : 'float-right pl-[100px]'}`}>
                     {/* <Image src={slice.image.uri} width={500} height={500} /> */}
                     <img src={slice.image.uri} />

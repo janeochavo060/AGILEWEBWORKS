92966e937aeff94abc5aa325dbf812b59aefa205
nicolumibao <nico.halcyondigital@gmail.com>
2023-02-08 08:18:14 +0800

    chore: build
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { props } from "@/lib/props/page";
 import dynamic from "next/dynamic";
 export const getStaticProps = props;
-
 const Homepage = ({ page, slices }) => {
   const sliceComponents = slices.map((e) => {
     let key = e?.key === "MainFeature2" ? e?.data?.main?.text : e?.key;

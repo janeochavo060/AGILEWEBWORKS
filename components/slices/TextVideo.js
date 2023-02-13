19f803b991af08c92db0e92ed434d168be260ccb
nicolumibao <nico.halcyondigital@gmail.com>
2023-02-13 16:09:13 +0800

    chore: add test collection
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- components/slices/TextVideo.js
+++ components/slices/TextVideo.js
@@ -1,7 +1,7 @@
 import YouTubeEmbed from "@/components/partials/YouTubeEmbed";
 
 export default function TextVideo({ slice, reverse }) {
-  // console.log(slice)
+  console.log(slice)
   // useEffect(() => {
   //   document.documentElement.style.setProperty('--triangle-color', 'red')
   // }, [])
@@ -9,15 +9,15 @@ export default function TextVideo({ slice, reverse }) {
   return (
     <div
       className={`w-[calc(100%-2rem)] lg:max-h-[600px] h-full lg:mx-0 grid grid-cols-1 lg:grid-cols-2 py-8 my-4 ${
-        reverse
+        slice?.main?.video_position === 'left'
           ? "pr-[6%] trapezoid-right float-left"
           : "pl-[6%] trapezoid-left float-right"
       }`}
-      style={{ backgroundColor: `${slice?.main?.b_g_color || "none"}` }}
+      style={{ backgroundColor: `${slice?.main?.bg_color || "none"}` }}
     >
       <div
         className={`p-4 md:p-8 flex flex-col justify-center items-start gap-4 ${
-          reverse ? "order-1" : "-order-1"
+          slice?.main?.video_position === 'left' ? "order-1" : "-order-1"
         }`}
       >
         <h2 className="font-bold text-white text-3xl">{slice?.main?.title}</h2>

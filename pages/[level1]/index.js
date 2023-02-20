496c164b6c1a69c7a06a4c31f97023e5aff15592
nicolumibao <nico.halcyondigital@gmail.com>
2023-02-20 17:50:30 +0800

    chore: update page
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

---
+++ pages/[level1]/index.js
@@ -0,0 +1,24 @@
+import dynamic from "next/dynamic";
+// import ParentPage from "@/components/page/ParentPage";
+import { paths, props } from "@/lib/props/level1";
+export const getStaticPaths = paths;
+export const getStaticProps = props;
+const Homepage = ({ page, slices }) => {
+  // return <ParentPage page={page} slices={slices} />
+  const sliceComponents = slices.map((e) => {
+    return dynamic(() => import("@/components/slices/" + e?.key));
+  });
+  return (
+    <>
+      {sliceComponents.map((SliceComponent, key) => {
+        let data = {
+          ...slices[key]?.data,
+          id: `${slices[key]?.key}-${key}`,
+          key: `${slices[key]?.key}`,
+        };
+        return <SliceComponent key={key} slice={data} />;
+      })}
+    </>
+  );
+};
+export default Homepage;

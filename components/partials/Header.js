a30f3848e167faa3f4220ef713f158c38f9fc127
nicolumibao <nico.halcyondigital@gmail.com>
2023-02-21 16:13:17 +0800

    chore: update metadata
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- components/partials/Header.js
+++ components/partials/Header.js
@@ -10,6 +10,9 @@ const Header = () => {
         <title>{metaData?.title || "Page"}</title>
         <meta name="description" content={metaData?.description || "Description"} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="type" property="og:image:type" content="png" />
+        <meta name="image" property="og:image" content={ metaData?.image ? `https://s3.ap-southeast-1.amazonaws.com/halcyon-agile-saas-platform-boilerplate/${metaData?.image}` : '/website.png'} />
+        <meta name="secure_url" property="og:image:secure_url" content={ metaData?.image ? `https://s3.ap-southeast-1.amazonaws.com/halcyon-agile-saas-platform-boilerplate/${metaData?.image}` : '/website.png'} />
         <link rel="icon" href={ metaData?.ico ? `https://s3.ap-southeast-1.amazonaws.com/halcyon-agile-saas-platform-boilerplate/${metaData?.ico}` : '/favicon.ico'} />
       </Head>
     </>

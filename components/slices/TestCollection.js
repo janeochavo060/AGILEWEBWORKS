ce766fc20f6766875a96f8138c3337fc4cf481e0
nicolumibao <nico.halcyondigital@gmail.com>
2023-02-23 21:19:39 +0800

    chore: add collection
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- components/slices/TestCollection.js
+++ components/slices/TestCollection.js
@@ -1,4 +1,16 @@
 export default function Slice({ slice }) {
-  console.log(slice, 'collection')
-  return "test";
+  const apps = slice?.main?.apps || []
+  // console.log(apps)
+  return (
+    <div className="max-w-screen-xl mx-auto w-full mt-[100px]">
+      <div className="gap-8 columns-3">
+        {apps.map((e, i) => (
+          <div key={i} className="border-2 w-full p-[10px]">
+            <p className="mb-[5px] font-bold text-[18px]">{e.attributes.title}</p>
+            <div className="" dangerouslySetInnerHTML={{__html: e.attributes?.data?.main?.content}} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }

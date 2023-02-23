53e349422cc92c5fb3758f91a1bae956edfcac83
nicolumibao <nico.halcyondigital@gmail.com>
2023-02-23 21:46:50 +0800

    chore: update collection
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- components/slices/TestCollection2.js
+++ components/slices/TestCollection2.js
@@ -1,6 +1,6 @@
 export default function Slice({ slice }) {
   // const apps = slice?.main?.apps || []
-  console.log(slice)
+  // console.log(slice)
   return (
     <div className="max-w-screen-xl mx-auto w-full mt-[100px]">
       <div className="gap-8 columns-3">

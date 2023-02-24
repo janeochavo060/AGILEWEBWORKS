3e1cc186428a8d136c1dcbf12b89ed811c000ad6
nicolumibao <nico.halcyondigital@gmail.com>
2023-02-24 13:58:29 +0800

    chore: add form
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- components/partials/Contact.js
+++ components/partials/Contact.js
@@ -1,38 +1,12 @@
-import FORMAPI from "@/lib/api/forms/request";
 import FormField from "@/components/forms/FormField";
 import Button from "@/components/forms/Button";
 import { useContext, Fragment } from "react";
 import { GlobalContext } from "@/lib/context/GlobalContext";
+import { formSubmit } from '@/lib/services/formService';
 export default function Contact() {
   const { form } = useContext(GlobalContext);
   const sections = form?.blueprint?.schema?.sections || [];
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    const payload = {};
-    sections.map((section) => {
-      let sectionPayload = {};
-      let fields = section?.fields || [];
-      fields.map(
-        (field) =>
-          (sectionPayload[field.state_name] = e.target[field.state_name].value)
-      );
-      payload[section?.state_name] = sectionPayload;
-    });
-
-    FORMAPI.submitForm('contact', payload)
-    .then((res) => {
-      console.log({ res });
-      e.target.reset();
-    })
-    .catch((err) => {
-      console.log({ err });
-    });
-  };
-
-  return !form ? (
-    <></>
-  ) : (
+  return (
     <div className="flex flex-col justify-center items-center max-w-95p md:max-w-2xl w-full mx-auto gap-8 bg-white py-12 px-4">
       {sections.map((section) => {
         const fields = section?.fields || [];
@@ -42,7 +16,7 @@ export default function Contact() {
               {section?.title}
             </h2>
             <form
-              onSubmit={(e) => onSubmit(e, section)}
+              onSubmit={(e) => formSubmit(e, 'contact', sections)}
               className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-lg w-full"
             >
               {fields.map((field) => (
--- components/slices/MainForm.js
+++ components/slices/MainForm.js
@@ -1,4 +1,46 @@
+import FORMAPI from "@/lib/api/forms/request"
+import Jsona from 'jsona';
+const dataFormatter = new Jsona();
+import FormField from "@/components/forms/FormField";
+import Button from "@/components/forms/Button";
+import { Fragment } from "react";
+import { formSubmit } from '@/lib/services/formService';
 export default function Slice({ slice }) {
-  // console.log(slice, 'main form')
-  return <>Form</>
+  const id = slice?.main?.form?.id
+  const {data} = FORMAPI.findFormsSwr(`/${id}?include=blueprint`, {
+    render: id
+  })
+  const form = dataFormatter.deserialize(data || {})
+  const sections = form?.blueprint?.schema?.sections || [];
+  return (
+    <div className="max-w-screen-xl mx-auto w-full mt-[120px]">
+      <div className="flex flex-col justify-center items-center max-w-95p md:max-w-2xl w-full mx-auto gap-8 bg-white py-12 px-4">
+        {sections.map((section) => {
+          const fields = section?.fields || [];
+          return (
+            <Fragment key={section?.state_name}>
+              <form
+                onSubmit={(e) => formSubmit(e, id, sections)}
+                className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-lg w-full"
+              >
+                {fields.map((field) => (
+                  <Fragment key={field?.state_name}>
+                    <FormField
+                      {...field}
+                      className={
+                        field?.state_name === "message" ? "md:col-span-2" : ""
+                      }
+                    />
+                  </Fragment>
+                ))}
+                <div className="md:col-span-2 text-center">
+                  <Button className="black-shadow" label="Submit" type="submit" />
+                </div>
+              </form>
+            </Fragment>
+          );
+        })}
+      </div>
+    </div>
+  )
 }

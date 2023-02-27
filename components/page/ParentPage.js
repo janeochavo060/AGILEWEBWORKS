8d8deb68a4c9f814e7f296f84a6c6b93070ade13
nicolumibao <nico.halcyondigital@gmail.com>
2023-02-27 17:00:49 +0800

    chore: update page
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- components/page/ParentPage.js
+++ components/page/ParentPage.js
@@ -2,7 +2,9 @@ import dynamic from "next/dynamic";
 import Header from "@/layout/partials/Header";
 export default function ParentPage ({page, slices}) {
   const sliceComponents = slices.map((e) => {
-    return dynamic(() => import("@/components/slices/" + e?.key));
+    return dynamic(() => import("@/components/slices/" + e?.key), {
+      loading: () => <div className="min-h-[100vh] w-full"></div>
+    });
   });
   // Push global component
   // Put it here to prevent displaying before the upper content

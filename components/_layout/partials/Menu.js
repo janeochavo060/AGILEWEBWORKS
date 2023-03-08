f7a0cd498668bd40a1a4f66f2fce2664f43419a1
nicolumibao <nico.halcyondigital@gmail.com>
2023-03-08 12:00:27 +0800

    chore: responsive
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- components/_layout/partials/Menu.js
+++ components/_layout/partials/Menu.js
@@ -36,7 +36,7 @@ export default function Menu ({ className }) {
   return (
     <div
       id="menu"
-      className={`${showSideMenu ? 'h-screen md:w-[50%] md:ml-[50%] lg:w-full lg:h-fit lg:ml-0' : ''} shadow-lg lg:shadow-none bg-white fixed pt-4 pb-3 z-[100] w-full transition-all duration-600 ease-in-out ${
+      className={`${showSideMenu ? 'h-screen md:w-[50%] md:ml-[50%] lg:w-full lg:h-fit lg:ml-0' : ''} bg-white fixed pt-4 pb-3 z-[100] w-full transition-all duration-600 ease-in-out ${
         className || ""
       } ${
         showMenu && scrollPosition === 0 ? `lg:bg-transparent` : "shadow-lg shadow-black/15 bg-white lg:px-8"

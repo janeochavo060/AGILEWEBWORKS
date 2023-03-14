d5ebaf2afc21fce6fa64539ff392e737d8d186fc
nicolumibao <nico.halcyondigital@gmail.com>
2023-03-15 07:20:16 +0800

    chore: update client list
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- components/slices/TextImageList.js
+++ components/slices/TextImageList.js
@@ -1,40 +1,40 @@
-import Image from "next/image"
-import React from "react"
-import TitleContentBlock from "../partials/TitleContentBlock"
-import { Slide } from "react-slideshow-image"
-import { useWindowSize } from "react-use"
+import Image from "next/image";
+import React from "react";
+import TitleContentBlock from "../partials/TitleContentBlock";
+import { Slide } from "react-slideshow-image";
+import { useWindowSize } from "react-use";
 
 export default function TextImageList({ slice }) {
-  const images = slice?.main?.images || []
-  const { width } = useWindowSize()
-  const isLaptop = width <= 1024 && width > 768
-  const isTablet = width <= 768 && width > 425
-  const isMobile = width <= 425 && width > 320
-  const isMobileSmall = width <= 320
+  const images = slice?.main?.images || [];
+  const { width } = useWindowSize();
+  const isLaptop = width <= 1024 && width > 768;
+  const isTablet = width <= 768 && width > 425;
+  const isMobile = width <= 425 && width > 320;
+  const isMobileSmall = width <= 320;
 
   const OurClientsSlideShow = () => {
     const properties = {
       duration: 3000,
       transitionDuration: 500,
       slidesToShow: isLaptop
-        ? 8
+        ? 5
         : isTablet
-        ? 6
+        ? 3
         : isMobile
         ? 2
         : isMobileSmall
         ? 1
-        : 10,
+        : 8,
       slidesToScroll: isTablet ? 3 : isMobile ? 1 : isMobileSmall ? 1 : 4,
       autoplay: false,
       arrows: slice?.main?.images?.length <= 8 ? false : true,
       canSwipe: slice?.main?.images?.length <= 8 ? false : true,
       indicators: false,
-    }
+    };
 
     return images.length ? (
       <div>
-        <div className="block justify-center m-2 sm:m-6">
+        <div className="block justify-center m-2 max-w-[90%] !m-auto sm:m-6">
           <Slide {...properties}>
             {images.map((image, i) => (
               <div key={i} className="flex justify-center m-4">
@@ -50,13 +50,13 @@ export default function TextImageList({ slice }) {
           </Slide>
         </div>
       </div>
-    ) : null
-  }
+    ) : null;
+  };
 
   return (
     <div className="flex flex-col text-center space-y-4 mt-16">
       <TitleContentBlock slice={slice} contentClass="max-w-xl px-4 mx-auto" />
       <OurClientsSlideShow />
     </div>
-  )
+  );
 }

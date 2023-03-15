9bee639f7b5bf052a0ee99af47884150c4c2cfe3
nicolumibao <nico.halcyondigital@gmail.com>
2023-03-15 11:34:39 +0800

    chore: update index
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- components/page/ParentPage.js
+++ components/page/ParentPage.js
@@ -1,9 +1,9 @@
 import dynamic from "next/dynamic";
 import Header from "@/components/_layout/partials/Header";
-export default function ParentPage ({page, slices, bottomComponent = false}) {
+export default function ParentPage({ page, slices, bottomComponent = false }) {
   const sliceComponents = slices.map((e) => {
     return dynamic(() => import("@/components/slices/" + e?.key), {
-      loading: () => <div className="min-h-[100vh] w-full"></div>
+      loading: () => <div className="min-h-[100vh] w-full"></div>,
     });
   });
   // Push global component
@@ -15,7 +15,7 @@ export default function ParentPage ({page, slices, bottomComponent = false}) {
     <>
       <Header meta={page?.metaData || {}} />
       {sliceComponents.map((SliceComponent, i) => {
-        const key = slices[i]?.key
+        const key = slices[i]?.key;
         const data = {
           ...slices[i]?.data,
           id: `${key}-${i}`,

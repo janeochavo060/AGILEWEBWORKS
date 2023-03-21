16836b2d67481e02988830ae1bc7c6e16193075f
nicolumibao <nico.halcyondigital@gmail.com>
2023-03-21 16:12:49 +0800

    chore: update collection
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -2,7 +2,6 @@ import ParentPage from "@/components/page/ParentPage";
 import { paths, props } from "@/lib/props/page";
 export const getStaticPaths = paths;
 export const getStaticProps = props;
-export default function DynamicPage ({ page, slices }) {
-  return <ParentPage page={page} slices={slices} />
-};
-
+export default function DynamicPage({ page, slices }) {
+  return <ParentPage page={page} slices={slices} />;
+}
--- pages/articles/[id].js
+++ pages/articles/[id].js
@@ -1,18 +1,21 @@
 import { paths, props } from "@/lib/props/articles";
 export const getStaticPaths = paths;
 export const getStaticProps = props;
-import ArticleBanner from '@/components/slices/ArticleBanner';
-export default function CollectionTemplate ({page}) {
-  const data = page?.data
+import ArticleBanner from "@/components/slices/ArticleBanner";
+import Header from "@/components/_layout/partials/Header";
+export default function CollectionTemplate({ page }) {
+  const data = page?.data;
   const customs = {
-    title: 'Current Events',
+    title: "Current Events",
     subtitle: page?.title,
-    date: page?.published_at
-  }
+    date: page?.published_at,
+  };
   return (
     <>
-      <ArticleBanner slice={{main: {...data?.main, ...data?.banner, ...customs}}} />
+      <Header meta={page?.metaData || {}} />
+      <ArticleBanner
+        slice={{ main: { ...data?.main, ...data?.banner, ...customs } }}
+      />
     </>
-  )
-};
-
+  );
+}

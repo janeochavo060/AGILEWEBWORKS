2e35bbd704d54df3ac8cac0e337930c20ddb7f47
nicolumibao <nico.halcyondigital@gmail.com>
2023-03-21 16:57:19 +0800

    chore: add content block
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- pages/articles/[id].js
+++ pages/articles/[id].js
@@ -2,7 +2,9 @@ import { paths, props } from "@/lib/props/articles";
 export const getStaticPaths = paths;
 export const getStaticProps = props;
 import ArticleBanner from "@/components/slices/ArticleBanner";
+import Content from "@/components/slices/Content";
 import Header from "@/components/_layout/partials/Header";
+import Link from "next/link";
 export default function CollectionTemplate({ page }) {
   const data = page?.data;
   const customs = {
@@ -16,6 +18,16 @@ export default function CollectionTemplate({ page }) {
       <ArticleBanner
         slice={{ main: { ...data?.main, ...data?.banner, ...customs } }}
       />
+      <Content slice={data} />
+      <div className="text-center">
+        <Link href="/current-events" title="Back">
+          <button
+            className={`bg-[#034F8B] font-bold uppercase text-xs px-8 text-white rounded-md h-[35px] mt-8 mb-16`}
+          >
+            BACK
+          </button>
+        </Link>
+      </div>
     </>
   );
 }

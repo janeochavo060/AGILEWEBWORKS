da041f928cf07116812f94f33e4f27ee29c702da
nicolumibao <nico.halcyondigital@gmail.com>
2023-08-30 14:37:35 +0800

    chore: update menu
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- components/_layout/partials/Menu.jsx
+++ components/_layout/partials/Menu.jsx
@@ -1,5 +1,15 @@
+import globalData from "@/lib/preBuildScripts/static/globalData.json";
 import Locales from "@/components/_layout/partials/Locales";
+import persistentStore from "@/lib/store/persistentStore";
 export default function Menu() {
+  const locale = persistentStore((state) => state.locale);
+  const { menus } = globalData;
+
+  const nodes = menus?.find((n) => n.locale === locale || "en") || [];
+
+  // console.log(nodes, "sdsd");
+
+  // console.log(menus, "sd");
   return (
     <div>
       Menu

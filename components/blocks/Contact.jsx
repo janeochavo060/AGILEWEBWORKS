6c773a8f075a21259bfed5681807caee27fbe24c
nicolumibao <nico.halcyondigital@gmail.com>
2023-08-31 12:13:27 +0800

    chore: simple fix
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- components/blocks/Contact.jsx
+++ components/blocks/Contact.jsx
@@ -2,7 +2,6 @@ import ContactForm from "@/components/partials/forms/ContactForm";
 export default function MainBanner({ block }) {
   return (
     <div className="m-auto max-w-[600px] mt-12 p-8 border-2 border-[#cacaca]">
-      sdsdsd
       <ContactForm form={block?.main?.form} />
     </div>
   );

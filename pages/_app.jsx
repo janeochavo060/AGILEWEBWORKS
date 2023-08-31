583a8804925ab3c7b36bc0187ed51efe40fa584c
nicolumibao <nico.halcyondigital@gmail.com>
2023-09-01 07:41:39 +0800

    chore: update localization
    Signed-off-by: nicolumibao <nico.halcyondigital@gmail.com>

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,19 +3,19 @@ import "@/styles/globals.css";
 import "@/styles/customs.css";
 import DefaultLayout from "@/components/_layout/DefaultLayout";
 import globalState from "@/lib/store/globalState";
+import persistentStore from "@/lib/store/persistentStore";
 import { useEffect } from "react";
 export default function App({ Component, pageProps }) {
+  const { page } = pageProps;
   useEffect(() => {
+    globalState.setState({ ready: true });
+    const locale = page?.locale;
+    if (locale) persistentStore.setState({ locale });
     const handleScroll = () => {
-      globalState.setState({
-        showLazy: true,
-      });
+      globalState.setState({ showLazy: true });
     };
     window.addEventListener("scroll", handleScroll, { passive: true });
-    globalState.setState({
-      ready: true,
-    });
-  }, []);
+  }, [page]);
   return (
     <div className="text-dim-black">
       <DefaultLayout>
